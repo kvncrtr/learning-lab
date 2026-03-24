699119569a8a3564e7d4d0c1df3d966e058ad02d
Kevin <console.logkevin@gmail.com>
2026-03-24 17:30:59 -0400

    in order for the no extra whitespaces to save on a value you have to point the value of .rstrip() to a new variable in memory

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
 name = 'There is no other name '
 name = name.rstrip()
 print(f"{name.upper()}:\n\tPeter\n\tJohn\n\tThomas")
+
+nostarch_url = 'https://nostarch.com'
+nostarch_url = nostarch_url.removeprefix('https://')
+print(nostarch_url)
