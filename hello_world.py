9a98288fdd96af663390a9e05015e37573f12773
Kevin <console.logkevin@gmail.com>
2026-03-24 17:49:44 -0400

    working on the excerises from the crash course 3rd edition

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,13 @@
+# Removing Whitespaces, var assignment, tabs new lines, and f-string 
 name = 'There is no other name '
 name = name.rstrip()
 print(f"{name.upper()}:\n\tPeter\n\tJohn\n\tThomas")
 
-nostarch_url = 'https://nostarch.com'
-nostarch_url = nostarch_url.removeprefix('https://')
+# Removing prefixes
+nostarch_url = 'apostle_paul.com'
+nostarch_url = nostarch_url.removeprefix('apostle')
 print(nostarch_url)
+
+# Escaping a famous quote
+quote = "Someone said \"The Earth is located the right distance from the Sun.\""
+print(quote)
