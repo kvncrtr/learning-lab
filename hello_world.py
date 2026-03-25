651ed320b9f03e7395fc203c713ee8de589e5879
Kevin <console.logkevin@gmail.com>
2026-03-25 09:33:46 -0400

    chore(python): End of learning chapter 2.
    - Learned about methods like, suffix, prefix, to remove whitespaces
    - Learned how to work with vars, and make constants in python.
    - Learned what explanatory comments to make your code easier to read.

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,42 @@
 # Removing Whitespaces, var assignment, tabs new lines, and f-string 
 name = 'There is no other name '
 name = name.rstrip()
-print(f"{name.upper()}:\n\tPeter\n\tJohn\n\tThomas")
+# print(f"{name.upper()}:\n\tPeter\n\tJohn\n\tThomas")
 
 # Removing prefixes
 nostarch_url = 'apostle_paul.com'
 nostarch_url = nostarch_url.removeprefix('apostle')
-print(nostarch_url)
+# print(nostarch_url)
 
 # Escaping a famous quote
 quote = "Someone said \"The Earth is located the right distance from the Sun.\""
-print(quote)
+# print(f"{quote} -- {name}")
+another_name = ' Andrew '
+# print(another_name.rstrip())
+# print(another_name.lstrip())
+# print(another_name.strip())
+
+file_name = 'python_notes.txt'
+# print(file_name.removesuffix('.txt'))
+
+# print(3 ** 2)
+
+universe_age = 14_000_000_000
+# print(universe_age)
+
+MY_FAV_NUM = 7
+# print(f"My favorite number is {MY_FAV_NUM}")
+
+""" 
+comms = 'yes this is my comment'
+print(comms) 
+"""
+
+GE = ['milk', 'eggs', 'water', 'yogurt']
+'''
+for food in GE:
+    print(food.title())
+'''
+
+# print("milk" in GE)
+# print(len(GE))
