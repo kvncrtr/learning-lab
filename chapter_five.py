317a09c4de18c627654124dae8387e8ffa12b99c
Kevin <console.logkevin@gmail.com>
2026-04-14 17:14:03 -0400

    chore(chapter six): Dictionaries
    - how to define a dictionary?
    
        very similar to a object in JS we will use curly braces and have a key value pair
    
        ```python
        alien_0 = {'color': 'green', 'points': 5}
        ```
    
    - How to access a value in a dictionary?
    
        we can use the dct name then the name of the key
    
        ```python
        alien_0['color']
        ```
    
    - How to assign a new value to a dct?
    
        you can use the dct name then the key’s name = value
    
        ```python
        alien_0['x_position'] = 0
        alien_0['y_position'] = 25
        ```
    
    - What are the order of the key value pairs naturally?
    
        a dct keeps it’s order from the time the pair was added
    
    - Can you overwrite values in a dct?
    
        yes you can use the same Key name then assign it a new value,
    
    - How can you delete a key value pair from a dct?
    
        use the `del` to remove the pair, check spelling and use the key to remove
    
        ```python
        del alien_0['points']
        ```
    
    - What is the difference from using a dct key access and the get method?
    
        if you try to access a key that’s not present then python will say something like
    
        ```python
        “Traceback (most recent call last):
          File "alien_no_points.py", line 2, in <module>
            print(alien_0['points'])
                  ~~~~~~~^^^^^^^^^^
        KeyError: 'points”
        ```
    
        vs
    
        if you use the `get()` on the key then you can get an error handled result saying
    
        ```python
         No [key name] value assigned
        ```
    
        here is how to use the method
    
        ```python
        “alien_0 = {'color': 'green', 'speed': 'slow'}
    
        point_value = alien_0.get('points', 'No point value assigned.')
        print(point_value)”
        ```

--- chapter_five.py
+++ chapter_five.py
@@ -1,8 +1,108 @@
-# If Statements
-cars = ['porche', 'maserati', 'tesla', 'lamborghini', 'mclaren']
-print('tesla' in cars)
-for car in cars:
-    if len(car) >= 7:
-        print('long')
-    else:
-        print('short')
+# # If Statements
+# cars = ['porche', 'maserati', 'tesla', 'lamborghini', 'mclaren']
+# print('tesla' in cars)
+# for car in cars:
+#     if len(car) >= 7:
+#         print('long')
+#     else:
+#         print('short')
+
+# # Conditional Test
+# car = 'Audi'
+# print(car.lower() == 'audi')
+
+# # Check inequality
+# requested_toppings = 'mushrooms'
+# if requested_toppings != 'anchovies':
+#     print('Hold the anchovies')
+
+# # Numerical Comparison
+# age = 18
+# print(age == 18)
+
+# if age != 19:
+#     print('You are not 19yo, sorry.')
+
+# if age == 18 and isinstance(age, (int, float)):
+#     print('This is a number that\'s 18 and an integer NOT a string')
+
+# if age != 18 or isinstance(age, (int, float)):
+#     print('this is a number')
+# else:
+#     print('never got to the int')
+
+# # Checking whether a value is in a list
+# requested_toppings = ['mushrooms', 'onions', 'pineapples']
+# print('mushrooms' in requested_toppings)
+# print('pepperonis' in requested_toppings)
+
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'kevin'
+
+# if user not in banned_users:
+#     print(f"{user.title()} you can post on this page, if you wish.")
+
+# car = 'subaru'
+# print(f"Is {car == 'subaru'}? I predict True.")
+# print(car == 'subaru')
+
+# print(f"\nIs {car == 'audi'}? I predict false.")
+# print(car == 'audi')
+
+# # Chaining if else and if
+# age_0 = 12
+
+# if age_0 < 4:
+#     price = 0
+# elif age_0 < 18:
+#     price = 25
+# else:
+#     price = 40
+
+# print(f"Your admission cost is ${price}.")
+
+# requested_toppings = ['mushrooms', 'extra cheese']
+
+# if 'mushrooms' in requested_toppings:
+#     print("Adding mushrooms.")
+# elif 'pepperoni' in requested_toppings:
+#     print("Adding pepperoni.")
+# elif 'extra cheese' in requested_toppings:
+#     print("Adding extra cheese.")
+
+# print("\nFinished making your pizza!")
+
+# # Usinf if statments with lists
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+# for request in requested_toppings:
+#     if request == 'green peppers':
+#         print('Sorry, no more green peppers')
+#     else:
+#         print(f"Adding {request} to the pizza.")
+
+# print("\nFinished making your pizza!")
+
+# requested_toppings = []
+
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         print(f"Adding {requested_topping}")
+#     print('Finihed making your pizza!')
+# else:
+#     print('Are you sure you want a plain pizza?')
+
+# available_toppings = ['mushrooms', 'olives', 'green peppers',
+#                       'pepperoni', 'pineapple', 'extra cheese']
+
+# requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+# for requested_topping in requested_toppings:
+#     if requested_topping in available_toppings:
+#         print(f"Adding {requested_topping}.")
+#     else:
+#         print(f"Sorry, we don't have {requested_topping}.")
+
+# print("\nFinished making your pizza!")
+
+# 
---
+++ chapter_six.py
@@ -0,0 +1,5 @@
+# CHAPTER 6
+# git add .; git commit; git push;    
+
+# Dictionary Def
+alien_0 = {'color':'green', 'points': 5}
