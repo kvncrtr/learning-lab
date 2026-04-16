60756ac235307046144c82ab8fb1d97fc465db7c
Kevin <console.logkevin@gmail.com>
2026-04-15 21:10:00 -0400

    feature(chapter seven): While Loop
    - The for loop takes a collection of items and executes a block of code
    once for each item in the collection. In contrast, the while loop runs
    as long as, or while, a certain condition is true.
    - In the previous example, we had the program perform certain tasks
    while a given condition was true. But what about more complicated
    programs in which many different events could cause the program to
    stop running?

---
+++ chapter_seven.py
@@ -0,0 +1,16 @@
+# Chapter 7
+# git add .; git commit; git push;
+
+# condition = True
+# while condition:
+#     print('Hello')
+#     break
+
+# def process_nums(nums):
+#     i = 0
+#     while i < 5:
+#         nums.pop()
+#         print(nums)
+#         i += 1
+
+# process_nums([1, 2, 3, 4, 5])
