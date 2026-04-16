67b1e9cf677592913b26235dbd91576a44bd5ce9
Kevin <console.logkevin@gmail.com>
2026-04-16 13:20:39 -0400

    feature(chapter eight): Text, Loops With Mods
    - What are functions?
    
        functions are reusable blocks of code that takes in an input and
        returns an predictable output based on the input.
    
    - What are the many ways to pass information to functions?
    
        define the function name and parameter
    
        ```python
        def message(name):
            print(f"""
            ~~~~~~~~ Message ~~~~~~~~
            Hey {name}, Im informing you that I am learning Python Crash
            Course 3rd Edition.
            """)
    
        message('Jr')
        message('Jeri')
        message('Koren')
        message('Jesus')
        ```
    
    - parameters and arguments what’re the differences?
    
        parameters are defined in the function definition arguments are the
        actual values passed into the function via a function call
    
    - what is positional arguments?
    
        the order of the parameters have to match sequentially with the
        arguments passed into the function.
    
        If you get funny results like this, check to make sure the order of
        the arguments in your function call matches the order of the
        parameters in the function’s definition.
    
        ```python
        def describe_pet(animal_type, pet_name):
        	"""Display information about a pet."""
        	print(f"\nI have a {animal_type}.")
        	print(f"My {animal_type}'s name is {pet_name.title()}.")
    
        describe_pet('hamster', 'harry')
    
        ```
    
    - what’re keyword arguments?
    
        you assign a parameter to an argument when calling a function so
        that there is no confusion.
    
    - what are default values?
    
        When writing a function, you can define a default value for each
        parameter. If an argument for a parameter is provided in the
        function call, Python uses the argument value. If not, it uses the
        parameter’s default value. So when you define a default value for a
        parameter, you can exclude the corresponding argument you’d usually
        write in the function call
    
    - how to make a copy of a list?
    
        you can use the `[:]` notation to make a new copy of an array
    
    - What happens when you remove an element from a list while iterating
    over it?
    
        When iterating over a list for a `for` loop it tracks the indexes
        internally. Inside the for loop when you modify a list the list
        shirks but the elements all shift left.
    
    - Why does removing an item cause elements to be skipped?
    
        When the list is modified rather shrink or enlarges the indexes are
        still a moving target within that particular list.
    
    - What does a `for` loop track internally when iterating over a list?
    
        `for` loops track indexes internally, verses a while loop is
        decoupled from the indexes.
    
    - Does a `for` loop re-evaluate the list after each mutation? (Yes/No + Why)
    
        No because it can only assess what is given and not what has been
        modified based on the shifting of indexes
    
    - If I remove index 0 from a list, what happens to the rest of the
    elements?
    
        the full list will shift indexes from right to left, at O(1)
    
    - Why does the loop move to the next index even after the list shrinks?
    
        it moves because the current index has tracked on that iteration
        and not the elements value
    
    - What are 3 safe ways to modify a list while iterating?
    
        using a while loop, for loop on a copy of the list, or build a
        new list

--- chapter_eight.py
+++ chapter_eight.py
@@ -13,24 +13,124 @@
 # message('Koren')
 # message('Jesus')
 
-# 8-3 T-Shirt
-def make_shirt(size, message, material):
-    print(f""" 
-        Shirt
-        * Price: FREE
-        * Shipping: $2.67
-        * Custom?: Yes
-        * Size: {size}
-        * Message: {message} 
-        * Made In: {material} 
-    """)
+# # 8-3 T-Shirt
+# def make_shirt(material, size='L', message='I Love Python'):
+#     print(f""" 
+#         Shirt
+#         * Price: FREE
+#         * Shipping: $2.67
+#         * Custom?: Yes
+#         * Size: {size}
+#         * Message: {message} 
+#         * Made In: {material} 
+#     """)
 
-make_shirt(
-    'XL', 
-    'Walk in the spirit and you shall put off the flesh',
-    'Cotton')
+# make_shirt(
+#     'Cotton',
+#     'XL', 
+#     'Walk in the spirit and you shall put off the flesh',
+# )
 
-make_shirt(
-    message='Walk in the spirit and you shall put off the flesh',
-    size='L', 
-    material='Cotton')
+# # 8-4 Large Shirts
+# make_shirt(material='Cotton')
+# make_shirt(size='M', material='Polyester')
+# make_shirt(message='Touch the Heavans', material='Polyester')
+
+# #8-5 Cities
+# def describe_city(city, country='USA'):
+#     print(f"""{city} is in {country}.""")
+
+# describe_city('Barcelona', 'Spain')
+# describe_city('New York')
+# describe_city('Los Angeles')
+
+# # 8-6 City Names
+# def city_country(city, country):
+#     return f"{city}, {country}"
+
+# print(city_country('Destin', 'USA'))
+# print(city_country('Queensland', 'Australia'))
+# print(city_country('Oaxaca', 'Mexico'))
+
+# # 8-7 Album
+# def make_album(artist, title, number_of_songs=5):
+#     data = {}
+#     data['artist'] = artist
+#     data['title'] = title
+#     if number_of_songs >= 5:
+#         data['number_of_songs'] = number_of_songs
+
+#     return data
+
+# print(make_album('Hillsong', 'Touch of Heaven', 1))
+# print(make_album('Elevation', 'So Be It', 10))
+# print(make_album('Red Rock Worship', 'Ascend', 19))
+
+# # 8-9 Messages
+# text = ['Love you too baby 💜', 'Babe I love you so much', 'Ok great', 'I got the email from Geico, also going to call around to 3 different credit unions to get some loan details']
+
+# def show_message(messages):
+#     for message in messages:
+#         print(f"""Message sent: {message}
+#         ________________________________
+#               """)
+        
+# show_message(text[:])
+
+# # 8-10 Messages
+# wifey = ['Do they have a microwave?', 'Ahhh sorry baby', 'Maybe get something from the vending machine if they have any.']
+# sent_messages = []
+
+# def send_messages(text_messages):
+#     while text_messages:
+#         message = text_messages.pop(0)
+#         print(f"""
+#         Message Sent: 
+#         {message}
+ 
+#         ~~~~~~~~~~~~~~~~~~~~~~~~
+#         """)
+#         sent_messages.append(message)
+
+# # send_messages(wifey[:])
+
+# # 8-11 Archived Messages
+# send_messages(wifey[:])
+# print(wifey)
+
+# # 8-12
+# ordered = []
+
+# def firehouse_subs(*contents):
+#     ordered.append(contents)
+
+# firehouse_subs('lettuce', 'ham', 'cheese')
+# firehouse_subs('olive oil', 'pickels')
+# firehouse_subs('salt', 'pepper', 'italian', 'smoke paprika', 'dry basil')
+# print(ordered)
+
+# # 8-13
+# def build_profile(first, last, **user_info):
+#     """Build a dictionary containing everything we know about a user."""
+#     print(user_info)
+#     user_info['first_name'] = first
+#     user_info['last_name'] = last
+#     return user_info
+
+# user_profile = build_profile('albert', 'einstein',
+#                              location='princeton',
+#                              field='physics')
+# user_profile_me = build_profile('kevin', 'carter',
+#                              location='atlanta',
+#                              field='software engineering',
+#                              married=True)
+# print(user_profile_me)
+
+# # 8-14
+# def make_car(make, model, **car_specs):
+#     car_specs['make'] = make
+#     car_specs['model'] = model
+#     return car_specs
+
+# car = make_car('subaru', 'outback', color='blue', tow_package=True)
+# print(car)
